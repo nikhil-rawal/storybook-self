ecce9db189a3b727ba1cfd894318a775dbb627d8
Nikhil Rawal <rawal.nikhil29@gmail.com>
2025-06-09 14:42:23 -0400

    next commit

--- stories/Header.jsx
+++ stories/Header.jsx
@@ -1,13 +1,18 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-import { Button } from './Button';
-import './header.css';
+import { Button } from "./Button";
+import "./header.css";
 
 export const Header = ({ user = null, onLogin, onLogout, onCreateAccount }) => (
   <header>
     <div className="storybook-header">
       <div>
-        <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+        <svg
+          width="32"
+          height="32"
+          viewBox="0 0 32 32"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <g fill="none" fillRule="evenodd">
             <path
               d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
@@ -23,7 +28,7 @@ export const Header = ({ user = null, onLogin, onLogout, onCreateAccount }) => (
             />
           </g>
         </svg>
-        <h1>Acme</h1>
+        <h1>Helo Acme</h1>
       </div>
       <div>
         {user ? (
@@ -36,7 +41,12 @@ export const Header = ({ user = null, onLogin, onLogout, onCreateAccount }) => (
         ) : (
           <>
             <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
+            <Button
+              primary
+              size="small"
+              onClick={onCreateAccount}
+              label="Sign up"
+            />
           </>
         )}
       </div>
